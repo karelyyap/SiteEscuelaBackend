955c4303fcd229ad530acab7b045eac8c8b55016
karelyyy a <you@example.com>
2026-05-26 18:57:18 -0700

    Limpieza profunda de node_modules y mariaDB

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({});
 
 module.exports = prisma;
